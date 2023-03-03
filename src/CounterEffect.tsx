19df638a142a8320ce894d75f0931914a037ff4c
eapepe0 <eapepe0@gmail.com>
2023-03-03 15:37:54 -0300

    Leccion 15 Finalizada

--- src/CounterEffect.tsx
+++ src/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { gsap } from "gsap";
 
 const MAX_COUNT = 10;
 export const CounterEffect = () => {
@@ -16,11 +17,18 @@ export const CounterEffect = () => {
       "%cSe llego al valor maximo!!!",
       "color:red; background-color:white"
     );
+    gsap.to("h2", { y: -10, duration: 0.2, ease: "ease.out" }).then(() => {
+      gsap.to("h2", { y: 0, duration: 1, ease: "bounce.out" });
+    });
+    //* creamos la animacion que afecta al h2 , donde y se movera hacia arriba con un valor negativo en un transcurso de 0.2 seg
+    //* gsap.to es una promesa asi que usando un .then podemos controlar lo que se ejecuta cuando se cumple la promesa
+    //* en este caso usamos otra animacion para simular un rebote
   }, [counter]); //* se ejecuta el useEffect cada vez que cambia el counter ( o sea cuando hacemos click en el boton)
 
   return (
     <>
-      <h1>CounterEffect : {counter}</h1>
+      <h1>CounterEffect :</h1>
+      <h2>{counter}</h2>
       <button onClick={handleClick}>+1</button>
     </>
   );
