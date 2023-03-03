911484c1c39217e0cd1b17c3d693bc12c1f1eaaa
eapepe0 <eapepe0@gmail.com>
2023-03-03 03:35:06 -0300

    Leccion 13 Finalizada

--- src/CounterBy.tsx
+++ src/CounterBy.tsx
@@ -4,24 +4,34 @@ interface Props {
   //* aca ponemos el tipo de las props que va a recibir el componente
   initialValue?: number; //* ? el signo de pregunta simboliza que podemos recibir o no la prop en el componente
 }
+
+//* asi debe lucir el counterState
+interface CounterState {
+  counter: number;
+  clicks: number;
+}
 export const CounterBy = ({ initialValue = 5 }: Props) => {
   //* aca ponemos que initialValue lo va a buscar en las definiciones que hacemos en interface Props
 
   //* definimos nuestro estado como un objeto que lo inicializamos de la siguiente forma
-  const [counterState, setCounterState] = useState({
+  //* en la parte de <CounterState> definimos como se debe ver nuestro state
+  const [{ counter, clicks }, setCounterState] = useState<CounterState>({
     counter: initialValue,
     clicks: 0,
   });
 
   //* extraemos de counterState
-  const { counter, clicks } = counterState;
+  //* nos podemos ahorrar este paso si desestructuramos en la creacion del useState
+  // const [counterState, setCounterState] = useState<CounterState>({...})
+  //const { counter, clicks } = counterState;
 
   //* una funcion que recibimos el valor value el cual es un numero
   const handleClick = (value: number) => {
     //* seteamos el counter el cual es un objeto de la siguiente forma
-    setCounterState((prev) => ({
-      counter: prev.counter + value,
-      clicks: prev.clicks + 1,
+    //* desestructuramos clicks y counter del estado
+    setCounterState(({ clicks, counter }) => ({
+      counter: counter + value,
+      clicks: clicks + 1,
     }));
   };
   return (
